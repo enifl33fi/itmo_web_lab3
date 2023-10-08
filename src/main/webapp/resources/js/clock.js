f63d4b79e39063a886d4712272b7c64853a0f29a
Kirill_Markov <enifl33fi@gmail.com>
2023-10-08 13:04:24 +0300

    something to start with

---
+++ src/main/webapp/resources/js/clock.js
@@ -0,0 +1,22 @@
+const hourHand = document.getElementById("hour-hand")
+const minuteHand = document.getElementById("minute-hand")
+const secondHand = document.getElementById("second-hand")
+
+function setDate() {
+    const curTime = new Date();
+
+    const seconds = curTime.getSeconds()
+    const minutes = curTime.getMinutes()
+    const hours = curTime.getHours() % 12
+
+    const secondDegrees = 90 + 360 * (seconds / 60)
+    secondHand.style.transform = `rotate(${secondDegrees}deg)`
+
+    const minuteDegrees = 90 + 360 * (minutes / 60) + 6 * (seconds / 60)
+    minuteHand.style.transform = `rotate(${minuteDegrees}deg)`
+
+    const hourDegrees = 90 + 360 * (hours / 12) + 30 * (minutes / 60)
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`
+}
+
+setInterval(setDate, 1000);
